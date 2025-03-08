10fdaf5977b01c896ef0534d8a44d8acbf41d838
Taek Seung Lee <dlxortmd987@gmail.com>
2025-03-08 17:53:21 +0900

    initial commit

---
+++ backend/src/test/java/com/project/blogmate/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.blogmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
