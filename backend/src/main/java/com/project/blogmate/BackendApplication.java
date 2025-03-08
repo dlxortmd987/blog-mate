e8ef3ec455b925895666fd990f80724157d6811f
Taek Seung Lee <dlxortmd987@gmail.com>
2025-03-09 01:28:32 +0900

    chore: import 정리

--- backend/src/main/java/com/project/blogmate/BackendApplication.java
+++ backend/src/main/java/com/project/blogmate/BackendApplication.java
@@ -1,11 +1,7 @@
 package com.project.blogmate;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BackendApplication {
