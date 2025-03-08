dedc26ebef9a4f9ea1787e77c954923b03366f17
Taek Seung Lee <dlxortmd987@gmail.com>
2025-03-09 03:12:48 +0900

    feat: (be) CORS 설정

---
+++ backend/src/main/java/com/project/blogmate/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.project.blogmate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost", "http://localhost:80")  // 브라우저가 접근하는 프론트엔드 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
