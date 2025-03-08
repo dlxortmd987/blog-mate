3598ae76c3ab4332ea262fc793ca8111d74de0b6
Taek Seung Lee <dlxortmd987@gmail.com>
2025-03-09 01:18:12 +0900

    feat: docker 환경 설정

--- backend/src/main/java/com/project/blogmate/BackendApplication.java
+++ backend/src/main/java/com/project/blogmate/BackendApplication.java
@@ -1,11 +1,14 @@
 package com.project.blogmate;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
